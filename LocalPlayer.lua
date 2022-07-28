8fc9bb8c39b55aeaa46cc9bd2dd9f2a3c18e96f8
KraftAffix <69615794+KraftAffix@users.noreply.github.com>
2022-07-28 18:58:51 +0800

    Update LocalPlayer.lua

--- LocalPlayer.lua
+++ LocalPlayer.lua
@@ -1,4 +1,4 @@
 game.Players.LocalPlayer:Kick([[
------You Have Been Banned For Cheating/Exploiting-----
------Time Remaining: 365 Days-----
+You Have Been Banned For Cheating/Exploiting
+Time Remaining: 365 Days
 ]])
